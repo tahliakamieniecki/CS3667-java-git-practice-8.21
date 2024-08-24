cd376af14e5e011dff771ea149056cbbdc275cbe
Tahlia Kamieniecki <tahliakamieniecki@Tahlias-MacBook-Air.local>
2024-08-24 12:11:03 -0400

    Main Method additions 8/24

--- animal.java
+++ animal.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 abstract class animal{
 
     public abstract void animalEats();
@@ -19,11 +22,31 @@ class Cat extends animal{
 
 class main{
     public static void main(String[] args) {
-    animal dog1 = new Dog ();
-    animal cat1 = new Cat();
+        animal dog1 = new Dog ();
+        animal cat1 = new Cat();
 
-    dog1.animalEats();
-    cat1.animalEats();
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter 'D' for Dog or 'C' for Cat");
+        char animal = S.next().charAt(0);
+
+        if(animal == 'D' || animal =='d')
+        {
+            dog1.animalEats();
+        }
+        else if(animal == 'C' || animal =='c')
+        {
+            cat1.animalEats();
+        }
+        else
+        {
+            System.out.println("Incorrect Input Please Enter a D or C");
+        }
+
+
+
+        
+        
+        
 
     }
 }
