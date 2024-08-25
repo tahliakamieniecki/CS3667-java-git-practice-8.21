54a02a18155d572291294b7e36f2d8a046528d1c
Tahlia Kamieniecki <tahliakamieniecki@Tahlias-MacBook-Air.local>
2024-08-25 19:09:19 -0400

    Finished for now 8/25

--- animal.java
+++ animal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-abstract class animal{
+ abstract class animal{
 
     public abstract void animalEats();
     
@@ -10,37 +10,49 @@ abstract class animal{
 
 class Dog extends animal{
     public void animalEats(){
-        System.out.println("A eats kibble");
+        System.out.println("A dog eats kibble");
+        
     }
 }
 
 class Cat extends animal{
     public void animalEats(){
         System.out.println("A cat eats wet food");
+        
     }
 }
 
+
+
 class main{
+    
     public static void main(String[] args) {
-        animal dog1 = new Dog ();
-        animal cat1 = new Cat();
+        
+    
+        animal a = new Dog();
+       
 
-        Scanner S = new Scanner(System.in);
-        System.out.println("Enter 'D' for Dog or 'C' for Cat");
-        char animal = S.next().charAt(0);
+        try (Scanner S = new Scanner(System.in)) {
+            System.out.println("Enter 'D' for Dog or 'C' for Cat");
+            char animal = S.next().charAt(0);
 
-        if(animal == 'D' || animal =='d')
-        {
-            dog1.animalEats();
+            if(animal == 'D' || animal =='d')
+            {
+                a = new Dog();
+                a.animalEats();
+            }
+            else if(animal == 'C' || animal =='c')
+            {
+                a = new Cat();
+                a.animalEats();
+            }
+            else
+            {
+                System.out.println("Incorrect Input Please Enter a D or C");
+            }
         }
-        else if(animal == 'C' || animal =='c')
-        {
-            cat1.animalEats();
-        }
-        else
-        {
-            System.out.println("Incorrect Input Please Enter a D or C");
-        }
+
+
 
 
 
